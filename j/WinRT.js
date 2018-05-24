284963d4897befbd68620c9e3cbc2e4497619114
Aaron Gustafson <aaron@easy-designs.net>
2018-05-24 15:10:40 -0700

    Adding process querying

--- j/WinRT.js
+++ j/WinRT.js
@@ -1,2 +1,20 @@
 $output.innerHTML += 'Added WinRT.js file\r\n';
 
+var process_list = window.Windows.System.Diagnostics.ProcessDiagnosticInfo.getForProcesses();
+
+for (let index in process_list) {
+	if (!process_list.hasOwnProperty(index)) continue;
+
+	let process_name = process_list[index].executableFileName;
+	switch (process_name) {
+		case 'Narrator.exe':
+			$output.innerHTML += 'Narrator is currently running\r\n';
+			break;
+		case 'jfw.exe':
+			$output.innerHTML += 'JAWS is currently running\r\n';
+			break;
+		case 'nvda.exe':
+			$output.innerHTML += 'NVDA is currently running\r\n';
+			break;
+	}
+}
